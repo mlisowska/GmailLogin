32b68cd5a187ecc8febde474997868025c9403d8
Marta Lisowska <lisowska.marta@outlook.com>
2019-11-23 16:30:55 +0100

    Gmail Login applied

---
+++ GmailLogin/Pages/LoginPage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GmailLogin.Pages
+{
+    public class LoginPage
+    {
+        public LoginPage(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        IWebDriver _webDriver;
+        
+        public IWebElement LoginInput { get => _webDriver.FindElement(By.Id("identifierId"));  }
+        public IWebElement LoginNext { get => _webDriver.FindElement(By.Id("identifierNext"));  }
+        public IWebElement PasswordInput { get => _webDriver.FindElement(By.XPath("//input[@type=\"password\" and @name=\"password\"]")); }
+    }
+}
---
+++ GmailLogin/TestScenarios/Login.cs
@@ -0,0 +1,54 @@
+﻿using GmailLogin.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace GmailLogin.TestScenarios
+{
+    public class Login
+    {
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver(@"C:\chromedriver_win32");
+            
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
+        }
+
+        IWebDriver _driver;
+        WebDriverWait _wait;
+
+        [Test]
+        public void TypeLogin()
+        {
+            _driver.Url = "https://gmail.com";
+            LoginPage loginPage = new LoginPage(_driver);
+
+            loginPage.LoginInput.SendKeys("testowanie678");
+            loginPage.LoginNext.Click();
+
+            _wait.Until(x => 
+            {
+                try
+                {
+                    return loginPage.PasswordInput.Enabled;
+                }
+                catch(StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+
+            loginPage.PasswordInput.SendKeys("ttt");
+            Assert.IsTrue(true);
+        }
+    }
+}
+
