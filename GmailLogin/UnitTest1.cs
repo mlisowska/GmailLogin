a2a671b5b944e6b663a1e5031c84bbd6edb4ee48
Marta Lisowska <lisowska.marta@outlook.com>
2019-11-23 15:04:56 +0100

    initial commit

---
+++ GmailLogin/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace GmailLogin
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
